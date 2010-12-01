20196aac989aedc2a37fce95e364964e93ad3d4a
Kristian Mandrup <kmandrup@gmail.com>
2010-12-01 22:11:34 +0100

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CreamAppMongoMapper::Application.config.secret_token = '079b8c5390b0b253e2e9253bcb7549c0ee70302f513708d122bdb8bbc984a6d0d72519cae88200add1897ae0a0c832e70448df23e268c9adb2680c8f53f13469'
